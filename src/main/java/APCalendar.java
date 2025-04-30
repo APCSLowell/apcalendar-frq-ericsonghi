a9e0252e65812a067df95a18bc6c8aaa16b749c5
Eric Song <146142807+ericsonghi@users.noreply.github.com>
2025-04-30 09:19:11 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -13,7 +13,7 @@ public class APCalendar
   public static int numberOfLeapYears(int year1, int year2)
   { 
     int x = 0;
-    for(int i = years1; i < years2; i++){
+    for(int i = year1; i < year2; i++){
         if(isLeapYear(i)){
             x++;
         }
